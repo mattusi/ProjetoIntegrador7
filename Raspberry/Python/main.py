d41c76d102d02041abe2bb4105414a2e4ca119b6
Matheus Tusi <matheus.tusi@itlean.com.br>
2021-03-08 13:02:39 -0300

    test

--- Raspberry/Python/main.py
+++ Raspberry/Python/main.py
@@ -81,8 +81,8 @@ while True:
         cv2.circle(frame_rgb, (points[0][1], start_height), 2, (255,0,0), -1)
         cv2.circle(frame_rgb, (int(middle), start_height), 2, (0,0,255), -1)
 
-        print(int((middle-320)/int(sys.argv[0])))
-        Direction(int((middle - 320)/float(sys.argv[0])))
+        print(int((middle-320)/int(sys.argv[1])))
+        Direction(int((middle - 320)/float(sys.argv[1])))
     else:
         start_height -= 5
         start_height = start_height % h

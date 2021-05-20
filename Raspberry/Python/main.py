26acf86a20ca919d0b16cfb8a5f83cd8980aba2a
Matheus Tusi <matheus.tusi@itlean.com.br>
2021-05-20 20:11:30 -0300

    optimizations

--- Raspberry/Python/main.py
+++ Raspberry/Python/main.py
@@ -34,17 +34,10 @@ while True:
         # extract the bounding box location of the barcode and draw
 		# the bounding box surrounding the barcode on the image
         (x, y, w, h) = barcode.rect
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        center = "x: " + str(x - w/2) + "y: " + str(y + h/2)
-        print(center)
 	   	# the barcode data is a bytes object so if we want to draw it
 		# on our output image we need to convert it to a string first
         barcodeData = barcode.data.decode("utf-8")
         barcodeType = barcode.type
-		# draw the barcode data and barcode type on the image
-        text = "{} ({})".format(barcodeData, barcodeType)
-        cv2.putText(frame, text, (x, y - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         
         print(barcodeData)
         print(h)
@@ -59,8 +52,6 @@ while True:
                     nextStopIndex = 0
                 FollowLine(50)
 
-		
-    cv2.imshow("Barcode Scanner", frame)
     key = cv2.waitKey(1) & 0xFF
  
 	# if the `q` key was pressed, break from the loop

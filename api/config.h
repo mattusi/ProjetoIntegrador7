8f5b7f6eca6c3ea4ce406c8cf5359b672385cb7f
Ewerson <ewerson@msn.com>
2021-05-20 21:57:03 -0300

    arquivos de configuração API

---
+++ api/config.h
@@ -0,0 +1,29 @@
+﻿/**
+ * @Author: Ewerson_Santos
+ * @Date:   2021-04-15T17:34:37-03:00
+ * @Filename: config.h
+ * @Last modified by:   ewerson
+ * @Last modified time: 2021-05-20T10:21:16-02:00
+ * @Copyright: (c) 2021 Ewerson Santos
+ */
+#ifndef config_h
+#define config_h
+
+// INCLUDES
+
+#include <HTTPClient.h>
+
+
+// DADOS UPLOAD
+// Mantenha este valor de chave de API para ser compatível com o código PHP fornecido na página do projeto.
+// Se você alterar o valor de apiKeyValue, o arquivo PHP /post-esp-data.php também precisa ter a mesma chave
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+//#define httpPort 80
+String host = "http://becsenac.life/api";
+
+// Variaveis Globais
+
+String veic_id = "1"; // Define o ID do Veiculo no DB.
+
+#endif
